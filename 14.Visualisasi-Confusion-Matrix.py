a2d542e41aff2b9843e72cd32ac2658ac1b60e8f
dadankatel <59067697+dadankatel@users.noreply.github.com>
2022-02-14 12:00:36 +0700

    Create 14.Visualisasi-Confusion-Matrix.py

---
+++ 14.Visualisasi-Confusion-Matrix.py
@@ -0,0 +1,28 @@
+"""
+Visualisasi Confusion Matrix
+
+Confusion matrix yang telah dihitung sebelumnya dapat divisualisasikan dengan menggunakan heatmap dari seaborn. 
+Untuk itu tampilkanlah visualisasi dari confusion matrix ini.
+"""
+# import required modules
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.clf()
+# name  of classes
+class_names = [0, 1] 
+fig, ax = plt.subplots()
+
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap='YlGnBu', fmt='g')
+ax.xaxis.set_label_position('top')
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.tight_layout()
+plt.show()
